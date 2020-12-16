92a72dabadce843ff7bd59d8277e3534ece2b401
Derrick Koo <derrick.koo@automattic.com>
2020-12-16 16:07:53 -0700

    feat: add Business Listing patterns

--- includes/class-newspack-listings-blocks.php
+++ includes/class-newspack-listings-blocks.php
@@ -19,11 +19,6 @@ defined( 'ABSPATH' ) || exit;
  */
 final class Newspack_Listings_Blocks {
 	/**
-	 * Slug for the block pattern category.
-	 */
-	const NEWSPACK_LISTINGS_BLOCK_PATTERN_CATEGORY = 'newspack-listings-patterns';
-
-	/**
 	 * The single instance of the class.
 	 *
 	 * @var Newspack_Listings_Blocks
@@ -48,8 +43,6 @@ final class Newspack_Listings_Blocks {
 	 */
 	public function __construct() {
 		add_action( 'enqueue_block_editor_assets', [ __CLASS__, 'manage_editor_assets' ] );
-		add_action( 'admin_init', [ __CLASS__, 'register_block_pattern_category' ], 10 );
-		add_action( 'admin_init', [ __CLASS__, 'register_block_patterns' ], 11 );
 		add_action( 'wp_enqueue_scripts', [ __CLASS__, 'custom_scripts' ] );
 		add_action( 'wp_enqueue_scripts', [ __CLASS__, 'custom_styles' ] );
 		add_action( 'init', [ __CLASS__, 'manage_view_assets' ] );
@@ -181,61 +174,6 @@ final class Newspack_Listings_Blocks {
 			);
 
 			wp_enqueue_style( 'newspack-listings-styles' );
-		}
-	}
-
-	/**
-	 * Register custom block pattern category for Newspack Listings.
-	 */
-	public static function register_block_pattern_category() {
-		return register_block_pattern_category(
-			self::NEWSPACK_LISTINGS_BLOCK_PATTERN_CATEGORY,
-			[ 'label' => __( 'Newspack Listings', 'newspack-listings' ) ]
-		);
-	}
-
-	/**
-	 * Register custom block patterns for Newspack Listings.
-	 * These patterns should only be available for certain CPTs.
-	 */
-	public static function register_block_patterns() {
-		// Block pattern config.
-		$block_patterns = [
-			'business' => [
-				'post_types' => [
-					Core::NEWSPACK_LISTINGS_POST_TYPES['marketplace'],
-					Core::NEWSPACK_LISTINGS_POST_TYPES['place'],
-				],
-				'settings'   => [
-					'title'       => __( 'Business Listing', 'newspack-listings' ),
-					'categories'  => [ self::NEWSPACK_LISTINGS_BLOCK_PATTERN_CATEGORY ],
-					'description' => _x(
-						'Business description, website and social media links, and hours of operation.',
-						'Block pattern description',
-						'newspack-listings'
-					),
-					'content'     => '<!-- wp:columns --><div class="wp-block-columns"><!-- wp:column {"width":66.66} --><div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph --><p>Consectetur a urna hendrerit scelerisque suspendisse inceptos scelerisque neque parturient a mi adipiscing euismod mus. Ad felis morbi magna augue consectetur eleifend sit sem habitant suspendisse posuere amet felis adipiscing a himenaeos ipsum vivamus dictum vestibulum lacus consectetur vestibulum erat dignissim per sem integer. Cras class ac adipiscing inceptos a enim porta a elit scelerisque tincidunt hac ad netus accumsan parturient conubia vestibulum nec quisque parturient interdum fringilla curabitur cras sociosqu interdum. Porta aenean id a mus consectetur lacus lacus ut parturient sapien ut a sociosqu potenti ridiculus non tristique cursus a at parturient condimentum a duis convallis per. Dictum elementum ultricies ac risus vestibulum adipiscing placerat imperdiet malesuada scelerisque dictum mus adipiscing a at at fermentum scelerisque nisl a dignissim suscipit sapien taciti nulla curabitur vestibulum.</p><!-- /wp:paragraph --></div><!-- /wp:column --><!-- wp:column {"width":33.33} --><div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:jetpack/map {"mapCenter":{"lng":-122.41941550000001,"lat":37.7749295},"mapHeight":null} -->
-					<div class="wp-block-jetpack-map" data-map-style="default" data-map-details="true" data-points="[]" data-zoom="13" data-map-center="{&quot;lng&quot;:-122.41941550000001,&quot;lat&quot;:37.7749295}" data-marker-color="red" data-show-fullscreen-button="true"></div>
-					<!-- /wp:jetpack/map --><!-- wp:jetpack/contact-info --><div class="wp-block-jetpack-contact-info"><!-- wp:jetpack/address /--><!-- wp:jetpack/email /--><!-- wp:jetpack/phone /--></div><!-- /wp:jetpack/contact-info --><!-- wp:buttons --><div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} --><div class="wp-block-button is-style-fill"><a class="wp-block-button__link" href="https://newspack.pub">Visit our website</a></div><!-- /wp:button --></div><!-- /wp:buttons --><!-- wp:social-links --><ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://newspack.pub","service":"wordpress"} /--><!-- wp:social-link {"url":"https://facebook.com","service":"facebook"} /--><!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /--><!-- wp:social-link {"url":"https://instagram.com","service":"instagram"} /--><!-- wp:social-link {"service":"linkedin"} /--><!-- wp:social-link {"service":"youtube"} /--></ul><!-- /wp:social-links --><!-- wp:separator {"className":"is-style-wide"} --><hr class="wp-block-separator is-style-wide"/><!-- /wp:separator --><!-- wp:heading {"level":4} --><h4>Hours of Operation</h4><!-- /wp:heading --><!-- wp:jetpack/business-hours /--></div><!-- /wp:column --></div><!-- /wp:columns -->',
-				],
-			],
-		];
-
-		/**
-		 * Register block patterns for particular post types. We need to get the post type using the
-		 * post ID from $_REQUEST since the global $post is not available inside the admin_init hook.
-		 * If we can't determine the current post type, just register the patterns anyway.
-		 */
-		$post_id           = isset( $_REQUEST['post'] ) ? sanitize_text_field( $_REQUEST['post'] ) : null; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		$current_post_type = ! empty( $post_id ) && function_exists( 'get_post_type' ) ? get_post_type( $post_id ) : null;
-
-		foreach ( $block_patterns as $pattern_name => $config ) {
-			if ( empty( $current_post_type ) || in_array( $current_post_type, $config['post_types'] ) ) {
-				$pattern = register_block_pattern(
-					'newspack-listings/' . $pattern_name,
-					$config['settings']
-				);
-			}
 		}
 	}
 }
--- newspack-listings.php
+++ newspack-listings.php
@@ -28,4 +28,5 @@ require_once NEWSPACK_LISTINGS_PLUGIN_FILE . '/includes/newspack-listings-utils.
 require_once NEWSPACK_LISTINGS_PLUGIN_FILE . '/includes/class-newspack-listings-settings.php';
 require_once NEWSPACK_LISTINGS_PLUGIN_FILE . '/includes/class-newspack-listings-core.php';
 require_once NEWSPACK_LISTINGS_PLUGIN_FILE . '/includes/class-newspack-listings-blocks.php';
+require_once NEWSPACK_LISTINGS_PLUGIN_FILE . '/includes/class-newspack-listings-block-patterns.php';
 require_once NEWSPACK_LISTINGS_PLUGIN_FILE . '/includes/class-newspack-listings-api.php';
