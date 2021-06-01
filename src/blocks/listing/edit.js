54a8bca8b8fd5aa3b85882662f2381f25d12ff3b
Derrick Koo <derrick.koo@automattic.com>
2021-06-01 14:15:44 -0600

    fix: cast listing IDs as strings to avoid breaking existing listings

--- src/blocks/listing/edit.js
+++ src/blocks/listing/edit.js
@@ -157,7 +157,7 @@ const ListingEditorComponent = ( {
 						if ( _listing.length ) {
 							setIsEditingPost( false );
 							setPost( null );
-							setAttributes( { listing: parseInt( _listing.shift().value ) } );
+							setAttributes( { listing: _listing.shift().value.toString() } );
 						}
 					} }
 					selectedPost={ isEditingPost ? null : listing }
