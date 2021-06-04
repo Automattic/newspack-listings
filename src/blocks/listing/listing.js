bdc8ae66e529d77d548ba950153f5fff7c2d5c82
Derrick Koo <derrick.koo@automattic.com>
2021-06-04 16:05:14 -0600

    refactor: response object with author and sponsors

--- src/blocks/listing/listing.js
+++ src/blocks/listing/listing.js
@@ -11,8 +11,15 @@ import { decodeEntities } from '@wordpress/html-entities';
 export const Listing = ( { attributes, error, post } ) => {
 	// Parent Curated List block attributes.
 	const { showAuthor, showCategory, showTags, showExcerpt, showImage, showCaption } = attributes;
-	const { meta } = post;
-	const { sponsors = false, author = false } = meta;
+	const {
+		author = '',
+		category = [],
+		excerpt = '',
+		media = {},
+		sponsors = false,
+		tags = [],
+		title = '',
+	} = post;
 
 	return (
 		<div className="newspack-listings__listing-post entry-wrapper">
@@ -21,38 +28,38 @@ export const Listing = ( { attributes, error, post } ) => {
 					{ error }
 				</Notice>
 			) }
-			{ showImage && post && post.media && post.media.image && (
+			{ showImage && post && media && media.image && (
 				<figure className="newspack-listings__listing-featured-media">
 					<img
 						className="newspack-listings__listing-thumbnail"
-						src={ post.media.image }
-						alt={ post.media.caption || post.title }
+						src={ media.image }
+						alt={ media.caption || title }
 					/>
-					{ showCaption && post.media.caption && (
+					{ showCaption && media.caption && (
 						<figcaption className="newspack-listings__listing-caption">
-							{ post.media.caption }
+							{ media.caption }
 						</figcaption>
 					) }
 				</figure>
 			) }
-			{ post && post.title && (
+			{ post && (
 				<div className="newspack-listings__listing-meta">
 					{ sponsors && 0 < sponsors.length && (
 						<span className="cat-links sponsor-label">
 							<span className="flag">{ sponsors[ 0 ].sponsor_flag }</span>
 						</span>
 					) }
-					{ showCategory && post.category.length && ! sponsors && (
+					{ showCategory && category.length && ! sponsors && (
 						<div className="cat-links">
-							{ post.category.map( ( category, index ) => (
+							{ category.map( ( _category, index ) => (
 								<Fragment key="index">
-									{ decodeEntities( category.name ) }
-									{ index + 1 < post.category.length && ', ' }
+									{ decodeEntities( _category.name ) }
+									{ index + 1 < _category.length && ', ' }
 								</Fragment>
 							) ) }
 						</div>
 					) }
-					<h3 className="newspack-listings__listing-title">{ decodeEntities( post.title ) }</h3>
+					<h3 className="newspack-listings__listing-title">{ decodeEntities( title ) }</h3>
 					{ sponsors && 0 < sponsors.length && (
 						<div className="newspack-listings__sponsors">
 							<span className="sponsor-logos">
@@ -89,15 +96,15 @@ export const Listing = ( { attributes, error, post } ) => {
 						<cite>{ __( 'By', 'newpack-listings' ) + ' ' + decodeEntities( author ) }</cite>
 					) }
 
-					{ showExcerpt && post.excerpt && <RawHTML>{ post.excerpt }</RawHTML> }
+					{ showExcerpt && excerpt && <RawHTML>{ excerpt }</RawHTML> }
 
-					{ showTags && post.tags.length && (
+					{ showTags && tags.length && (
 						<p>
 							<strong>{ __( 'Tagged: ', 'newspack-listings' ) }</strong>
-							{ post.tags.map( ( tag, index ) => (
+							{ tags.map( ( tag, index ) => (
 								<Fragment key="index">
 									{ decodeEntities( tag.name ) }
-									{ index + 1 < post.tags.length && ', ' }
+									{ index + 1 < tags.length && ', ' }
 								</Fragment>
 							) ) }
 						</p>
