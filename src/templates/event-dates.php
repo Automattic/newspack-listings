3782460332a555514d9739aac81312a6fc6c4ee6
Laurel Fulford <laurel.fulford@automattic.com>
2023-08-18 08:45:23 -0700

    feat: add internationalization to event dates

--- src/templates/event-dates.php
+++ src/templates/event-dates.php
@@ -38,11 +38,11 @@ call_user_func(
 			}
 		}
 
-		$the_start_date = $start_date->format( $start_date_format );
+		$the_start_date = date_i18n( $start_date_format, strtotime( $start_date->format( $start_date_format ) ) );
 		$the_end_date   = '';
 
 		if ( ! empty( $end_date ) ) {
-			$the_end_date = $end_date->format( $end_date_format );
+			$the_end_date = date_i18n( $end_date_format, strtotime( $end_date->format( $end_date_format ) ) );
 		}
 		?>
 	<div class="newspack-listings__event-dates">
@@ -54,7 +54,7 @@ call_user_func(
 
 			<?php if ( $show_time ) : ?>
 				<span class="newspack-listings__event-date-time">
-					<?php echo esc_html( $start_date->format( $time_format ) ); ?>
+					<?php echo date_i18n( $time_format, strtotime( $start_date->format( $time_format ) ) ); ?>
 				</span>
 			<?php endif; ?>
 		</time>
@@ -75,7 +75,7 @@ call_user_func(
 				?>
 				<?php if ( $show_time ) : ?>
 					<span class="newspack-listings__event-time">
-						<?php echo esc_html( $end_date->format( $time_format ) ); ?>
+						<?php echo date_i18n( $time_format, strtotime( $end_date->format( $time_format ) ) ); ?>
 					</span>
 				<?php endif; ?>
 			</time>
