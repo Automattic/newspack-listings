325680b66b1081958869cb47a851cf730d84f4f9
dkoo <derrick.koo@automattic.com>
2024-07-02 14:03:21 -0600

    fix: phpcs errors

--- includes/importer/importer-utils.php
+++ includes/importer/importer-utils.php
@@ -8,16 +8,17 @@
 namespace Newspack_Listings\Importer_Utils;
 
 /**
- * Format the data from CSV.
+ * Format the data from CSV as UTF-8.
  *
  * @since 1.0
  * @param string $data Data to be formatted.
  * @param string $file_encoding File encoding of the data.
  */
 function format_data( $data, $file_encoding = 'UTF-8' ) {
-	// Check if the function utf8_encode exists. The function is not found if the php-xml extension is not installed on the server.
-	$return = function_exists( 'utf8_encode' ) ? utf8_encode( $data ) : $data;
-	return ( 'UTF-8' == $file_encoding ) ? $data : $return;
+	if ( ! function_exists( 'mb_convert_encoding' ) || ! function_exists( 'mb_detect_encoding' ) ) {
+		return $data;
+	}
+	return mb_convert_encoding( $data, $file_encoding, mb_detect_encoding( $data, 'auto' ) );
 }
 
 /**
